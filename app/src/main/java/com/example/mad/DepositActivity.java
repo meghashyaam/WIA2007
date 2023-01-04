f0823c09c2e5affeee748cd960e5ed334264d530
ChewanArRayyan <u2000531@siswa.um.edu.my>
2023-01-05 02:07:30 +0800

    ui done i think

--- app/src/main/java/com/example/mad/DepositActivity.java
+++ app/src/main/java/com/example/mad/DepositActivity.java
@@ -3,23 +3,17 @@ package com.example.mad;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.ui.AppBarConfiguration;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class BookActivity extends AppCompatActivity {
+public class DepositActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book);
+        setContentView(R.layout.activity_deposit);
 
         Toolbar toolbar = findViewById(R.id.materialToolbar);
         TextView toolbartitle = findViewById(R.id.toolbar_title);
@@ -37,20 +31,5 @@ public class BookActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-
-        //change press color for buttonBookStudyCube
-        Button buttonBookStudyCube = findViewById(R.id.buttonBookStudyCube);
-        buttonBookStudyCube.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                buttonBookStudyCube.setSelected(!buttonBookStudyCube.isSelected());
-            }
-        });
-
     }
-
-
-
-
-
 }
